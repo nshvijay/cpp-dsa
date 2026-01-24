13a2274ebedde57fda8d001e551e00166f893fc4
nshvijay <vijaypalkumawat2020@gmail.com>
2026-01-24 22:13:53 +0530

    datatype

---
+++ dsa/00_jump/02_datatype.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main(){
+
+     char grade = 'A';
+     string phrase = "Mathematics and Computing";
+     int age =50;
+     double sgpa1=7.9;
+     bool male=true; 
+     cout << grade << endl;
+
+    return 0;
+}
