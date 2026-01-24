a04338e7e1d6e7a2f611f811eb296029c5d0e552
nshvijay <vijaypalkumawat2020@gmail.com>
2026-01-24 21:21:48 +0530

    start

---
+++ dsa/00_jump/00_change.cpp
@@ -0,0 +1,14 @@
+//yeah so I'm moving from C to C++ so this basics is just for the jump
+// main task is DSA
+//Let's start
+
+#include <iostream>
+ using namespace std;
+ int main(){
+ cout<< "Hello" << endl;
+ cout<< "    /|" <<endl;
+ cout<< "   / |" <<endl;
+ cout<< "  /  |" <<endl;
+ cout<< " /___|" <<endl;
+ return 0;
+}
---
+++ dsa/00_jump/01_variables.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main(){
+    string name= "Vijay";
+    int age=19;
+    cout << "There once was a man named Vijay" << endl;
+    cout << "He was 19 years old" << endl;
+    cout << "He liked being 19" << endl;
+    cout << "But he didn't like the name Vijay" << endl;
+
+// alter method
+    cout << "My name is " << name << endl;
+    cout << "My age is " << age << endl;
+
+
+
+
+    return 0;
+}
