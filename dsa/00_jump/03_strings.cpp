cfd9064a0109b4bd2d934208bcd25d3ce9cea273
nshvijay <vijaypalkumawat2020@gmail.com>
2026-01-25 21:05:52 +0530

    strings

---
+++ dsa/00_jump/03_strings.cpp
@@ -0,0 +1,14 @@
+//playing with strings
+#include <iostream>
+using namespace std;
+int main(){
+    cout <<"Hello"<< endl ;
+    cout <<"Vijay"<< endl ;// here both will be printed in different line as endl ends the line
+
+    cout <<"Hello " ;
+    cout <<"Vijay"<< endl ; // both will be printed in same line as endl is not there 
+
+
+
+    return 0;
+}
