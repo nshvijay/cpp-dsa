fe400262ca92573ffce9a3022c330719b6c38ccb
nshvijay <vijaypalkumawat2020@gmail.com>
2026-02-01 14:13:29 +0530

    more

---
+++ dsa/01_c++.cpp/01_more.cpp
@@ -0,0 +1,21 @@
+//going from c to c++, fast pace.
+#include <iostream>
+using namespace std;
+int main(){
+// comments same as c
+    int x = 5;
+    cout<< x <<endl; 
+    float y =7;
+    cout<< y/x <<endl;
+    // cin used as input taking keyword
+    int z;
+    cin>> z;
+    cout<< z <<endl;
+    cout<< 2*3/4 <<endl;
+    cout<< 3/4*2 <<endl;
+    float w = (float)x;
+    cout<< w + 0.5;
+
+
+    return 0;
+}
